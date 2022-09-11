e98960facb2172f50959bb55b045791614261946
str4ng3love <normalemailadress@gmail.com>
2022-09-11 12:58:05 +0200

    footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,68 +1,83 @@
-import { SiFacebook } from 'react-icons/si'
-import next from '../public/images/logotypes/next.js/dark-background/nextjs-logotype-dark-background.png'
-import vercel from '../public/images/logotypes/vercel/light/vercel-logotype-light.png'
-import wreck from '../public/images/wreck.png'
 import Link from 'next/link'
 import { useContext } from 'react'
 import { LangContext } from '../context/state'
 import stylesFooter from '../styles/Footer.module.css'
 import Image from 'next/image'
+import next from '../public/images/logotypes/next.js/dark-background/nextjs-logotype-dark-background.png'
+import vercel from '../public/images/logotypes/vercel/light/vercel-logotype-light.png'
+import wreck from '../public/images/wreck.png'
+import { SiFacebook } from 'react-icons/si'
+import { TbArrowBigTop, TbArrowBigUpLine, TbArrowBigUpLines} from 'react-icons/tb'
+
 const Footer = () => {
     const {lang} = useContext(LangContext)
   return (
     <footer className={stylesFooter.footer}>
         {lang==='PL'?
-        <div className={stylesFooter.footerContainer}>
-            <div className={stylesFooter.footerLinks}>
-            <ul>
-                 <li>
-                    <Link href="/">Discord</Link>
-                </li>
-                <li>
-                    <Link href="/">O nas</Link>
-                </li>
-                <li>
-                    <Link href="/">Główna</Link>
-                </li>
-            </ul>
-            <ul>
-                <li><a href="https://www.facebook.com/uselessidea.co/"><SiFacebook/> Facebook</a></li>
-                <li><a href="https://zkillboard.com/corporation/98067874/"><Image src={wreck} /> zKillboard</a></li>
-            </ul>
-            </div>
+        <div className={stylesFooter.Container}>
             <div className={stylesFooter.tm}>
                 <span className={stylesFooter.nextspan}><a href="https://nextjs.org"> Built with <Image height={15} width={90} src={next}/></a></span>
                 <span className={stylesFooter.nextspan}><a href="https://vercel.com"> Powered by <Image height={15} width={90} src={vercel}/></a></span>
             </div>
+            <div className={stylesFooter.Links}>
+                <ul>
+                    <li>
+                        <Link href="/">Discord</Link>
+                    </li>
+                    <li>
+                        <Link href="/">O nas</Link>
+                    </li>
+                    <li>
+                        <Link href="/">Główna</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li><a href="https://www.facebook.com/uselessidea.co/"><SiFacebook/> Facebook</a></li>
+                    <li><a href="https://zkillboard.com/corporation/98067874/"><Image src={wreck} /> zKillboard</a></li>
+                </ul>
+            </div>
+            <span onClick={(e)=>{
+                window.scrollTo({behavior: 'smooth',
+                top: 0
+            })
+            }}
+            className={stylesFooter.arrow}><TbArrowBigTop id={stylesFooter.ar1} size={50} /><TbArrowBigUpLine id={stylesFooter.ar2}  size={50}/> <TbArrowBigUpLines id={stylesFooter.ar3}  size={50} /></span>
+         
         </div>
         :
-        <div className={stylesFooter.footerContainer}>
-        <div className={stylesFooter.footerLinks}>
-        <ul>
-             <li>
-                <Link href="/">Discord</Link>
-            </li>
-            <li>
-                <Link href="/">About</Link>
-            </li>
-            <li>
-                <Link href="/">Home</Link>
-            </li>
-        </ul>
-        <ul>
-            <li><a href="https://www.facebook.com/uselessidea.co/"><SiFacebook/> Facebook</a></li>
-            <li><a href="https://zkillboard.com/corporation/98067874/"><Image src={wreck} /> zKillboard</a></li>
-        </ul>
+        <div className={stylesFooter.Container}>
+              <div className={stylesFooter.tm}> 
+                <span className={stylesFooter.nextspan}>
+                    <a href="https://nextjs.org">Built with <Image height={20} width={120} src={next}/></a>
+                </span>
+                <span className={stylesFooter.nextspan}>
+                    <a href="https://vercel.com"> Powered by <Image height={20} width={120} src={vercel}/></a>
+                </span>
+            </div>
+
+            <div className={stylesFooter.Links}>
+                <ul>
+                    <li>
+                        <Link href="/">Discord</Link>
+                    </li>
+                    <li>
+                        <Link href="/">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li><a href="https://www.facebook.com/uselessidea.co/"><SiFacebook/> Facebook</a></li>
+                    <li><a href="https://zkillboard.com/corporation/98067874/"><Image src={wreck} /> zKillboard</a></li>
+                </ul>
+            </div>
+            <span onClick={(e)=>{
+                window.scrollTo({behavior: 'smooth',
+                top: 0
+            })
+            }} className={stylesFooter.arrow}><TbArrowBigTop id={stylesFooter.ar1} size={50} /><TbArrowBigUpLine id={stylesFooter.ar2}  size={50}/> <TbArrowBigUpLines id={stylesFooter.ar3}  size={50} /></span>
         </div>
-        <div className={stylesFooter.tm}> 
-            <span className={stylesFooter.nextspan}>
-                <a href="https://nextjs.org">Built with <Image height={20} width={120} src={next}/></a>
-            </span>
-            <span className={stylesFooter.nextspan}>
-                <a href="https://vercel.com"> Powered by <Image height={20} width={120} src={vercel}/></a>
-            </span>
-        </div>
-    </div>
         }  
     </footer>
   )
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,17 +2,28 @@ import Meta from "./Meta"
 import Header from '../components/Header'
 import Footer from "./Footer"
 import stylesLayout from '../styles/Layout.module.css'
+import Script from "next/script"
 
 const Layout = ({children}) => {
   return (
     <>
     <Meta />
     <Header />
-      <div className={stylesLayout.container}>
-        {children}
-       
-      </div> 
+    <div className={stylesLayout.bg}>
+        <div className={stylesLayout.container}>
+          {children}
+        
+        </div> 
+
+    </div>
     <Footer />
+    <Script id="resize-bg-onScroll"
+    strategy="lazyOnload"
+    dangerouslySetInnerHTML={{
+      __html: `
+        
+      `
+    }}   />
     </>
   )
 }
