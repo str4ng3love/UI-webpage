bf616ae5eeda02a6c14175c10685825da635914a
str4ng3love <normalemailadress@gmail.com>
2022-09-11 19:13:03 +0200

    minor tweaks

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,9 +1,20 @@
-
+import { LangContext } from "../context/state"
+import { useContext } from "react"
+import Meta from "../components/Meta"
 
 const About = () => {
+const lang = useContext(LangContext)
+
   return (
     <>
-        
+        {lang === `PL`? 
+        <>
+          <Meta title='Useless Idea | O nas'></Meta>
+        </>
+        :
+        <>
+          <Meta title='Useless Idea | About'></Meta>
+        </>}
     </>
   )
 }
