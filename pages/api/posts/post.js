ef10ad68b3a99261d370d762397439ff2487ccd1
str4ng3love <normalemailadress@gmail.com>
2022-09-20 14:19:21 +0200

    tweaking...

--- pages/api/posts/post.js
+++ pages/api/posts/post.js
@@ -4,16 +4,16 @@ import Post from "../../../models/Post"
 export default async function handler (req, res) {
     const query = req.query
 
-  res.json({post: 'kachaa'})
 
-    // try {
-    //     const post = await Post.findOne({title: query.post})
 
-    //     res.json({post})
-    // } catch (error) {
-    //     if(error){
-    //         console.log(error)
-    //         res.json({error})
-    //     }
-    // }
+    try {
+        const post = await Post.findOne({title: query.post})
+
+        res.json({post})
+    } catch (error) {
+        if(error){
+            console.log(error)
+            res.json({error})
+        }
+    }
 }
