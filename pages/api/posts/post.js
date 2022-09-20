d0db8885db5d84ad298cd290916b3437b0c7e60d
str4ng3love <normalemailadress@gmail.com>
2022-09-20 14:36:14 +0200

    tweaking.

--- pages/api/posts/post.js
+++ pages/api/posts/post.js
@@ -1,21 +1,15 @@
 import Post from "../../../models/Post"
-
+import ConnectDB from "../../../lib/ConnectDB"
 
 export default async function handler (req, res) {
     const query = req.query
-
+    await ConnectDB()
 
 
     try {
         const post = await Post.findOne({title: query.post})
 
-        res.json({post: {
-            title: ` dziala`,
-            meta: {
-                author: `zenek`,
-                createdAt: `teraz `
-            }
-        } })
+        res.json({post})
     } catch (error) {
         if(error){
             console.log(error)
